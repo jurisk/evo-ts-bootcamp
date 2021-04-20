538cb4885b81c65598ae96d04f06661a251b151a
Juris K <jurisk@@users.noreply.github.com>
2021-04-20 08:38:33 +0300

    First attempt at mergeSort

--- homeworks/03-data-structures/src/binary-tree.test.ts
+++ homeworks/03-data-structures/src/binary-tree.test.ts
@@ -1,5 +1,21 @@
-import {example} from "./binary-tree";
+import {mergeSort} from "./binary-tree";
 
-test('test to be test', () => {
-    expect(example).toBe("test");
+/* - Develop the `mergeSort` function. */
+test('mergesort should sort', () => {
+    const randomArray = Array(1000).fill(0).map(() => Math.random())
+    const compareFn = (a, b) => a - b
+    const expected = [...randomArray].sort(compareFn)
+    const obtained = mergeSort(randomArray, compareFn)
+
+    expect(expected).toEqual(obtained);
 });
+
+// TODO:
+// - Implement `BinaryTree` interface.
+// - Develop `BinaryTree` class that implements `BinaryTree` interface.
+// - `BinaryTree` class has to be generic and able to define a node value.
+// - Implement `BinarySearchTree` interface that extends `BinaryTree` interface.
+// - Develop `BinarySearchTree` class that implements `BinarySearchTree` interface
+// and extends `BinaryTree` class.
+// - `BinarySearchTree` class has to have a numeric node value (not a generic class).
+// - Cover all the code with tests (via `jest`). Coverage has to be 100%.
--- homeworks/03-data-structures/src/binary-tree.ts
+++ homeworks/03-data-structures/src/binary-tree.ts
@@ -1,1 +1,53 @@
-export const example = "test"
+interface TreeNode<T> {
+    value: T
+    left: TreeNode<T>
+    right: TreeNode<T>
+}
+
+enum TraverseType {
+    DfsInOrder,
+    DfsPreOrder,
+    DfsPostOrder,
+    Bfs,
+}
+
+interface BinaryTree<T> {
+    constructor(tree: TreeNode<T>): void;
+    setTree(value): this;
+    traverse(traverseType: TraverseType): T[];
+    getColumn(columnOrder: number): T[];
+}
+
+interface BinarySearchTree extends BinaryTree<number> {
+    has(value: number): boolean;
+}
+
+type CompareFunction<T> = (a: T, b: T) => number
+type MergeSort<T> = (array: readonly T[], compareFunction: CompareFunction<T>) => readonly T[]
+
+export const mergeSort = <T>(array: readonly T[], compareFunction: CompareFunction<T>): readonly T[] => {
+    /** Ordered merging of two ordered lists */
+    function merge(a: readonly T[], b: readonly T[]): readonly T[] {
+        if (a.length === 0) {
+            return b
+        } else if (b.length === 0) {
+            return a
+        } else {
+            const [ah, at] = [a[0], a.slice(1)]
+            const [bh, bt] = [b[0], b.slice(1)]
+            const comparison = compareFunction(ah, bh)
+            return comparison < 0 ? [ah, ...merge(at, b)] : [bh, ...merge(a, bt)]
+        }
+    }
+
+    if (array.length <= 1) {
+        return array
+    } else {
+        const half = Math.ceil(array.length / 2);
+
+        const sortedFirstHalf = mergeSort(array.slice(0, half), compareFunction)
+        const sortedSecondHalf = mergeSort(array.slice(half), compareFunction)
+
+        return merge(sortedFirstHalf, sortedSecondHalf)
+    }
+}
